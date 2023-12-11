de2f8cb7b551c9315191143d20b6c0707e8e5ee0
hakazenana <wangzhiheng994@mail.ustc.edu.cn>
2023-12-10 21:35:04 -0500

    add boundary terms and update b

--- src/global.h
+++ src/global.h
@@ -5,25 +5,29 @@ const double invhsq = 1/h/h;
 const double tau = 0.01; // timestep size ht
 const double endT = 1; // end time
 
-// boundary condtions on top
-double a(double x)
-{
-    return 3;
-}
-// boundary condtions on left
-double b(double y)
-{
-    return 3;
-}
-// boundary condtions on bottom
-double c(double y)
-{
-    return 10;
-}
-// boundary conditions on right
-double d(double x)
-{
-    return 10;
-}
+#define a(x) 3
+#define b(y) 3
+#define c(y) 10
+#define d(x) 10 
+// // boundary condtions on top
+// double a(double x)
+// {
+//     return 3;
+// }
+// // boundary condtions on left
+// double b(double y)
+// {
+//     return 3;
+// }
+// // boundary condtions on bottom
+// double c(double y)
+// {
+//     return 10;
+// }
+// // boundary conditions on right
+// double d(double x)
+// {
+//     return 10;
+// }
 
 #endif
